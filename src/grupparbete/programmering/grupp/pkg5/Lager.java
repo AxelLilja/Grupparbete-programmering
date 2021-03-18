19775baf0e15612bf4503242b241a7c6752ba1fb
Skysurfer65 <richard_fehling@hotmail.com>
2021-03-18 10:22:52 +0100

    Final??

--- src/grupparbete/programmering/grupp/pkg5/Lager.java
+++ src/grupparbete/programmering/grupp/pkg5/Lager.java
@@ -63,6 +63,7 @@ public class Lager {
             System.out.println("Tyvärr. Vi har inga bilar i lager att sälja idag");
         } 
         else {
+            System.out.println("");
             int j = 0;
             //Dynamisk lista för bilar i lager
             for (Bil i: Lager.listaBilar){
